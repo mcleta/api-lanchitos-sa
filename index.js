e3a5dc1d98b6418113b6508c14b88852f0fc7769
Murillo C Leta <murillocasemiro@gmail.com>
2021-12-01 18:38:31 -0300

    Criação da rota pedido

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const chatFuel = require('chatfuel-api');
 
 const app = express();
-
 
 app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server's up in door ${process.env.PORT}`));
 
 app.get("/cardapio", function(request, response){
-  const {name} = request.headers
+  const {name} = request.headers;
   response.status(200).json(
     {
       "messages": [
-        {"text": `Olá ${name}!` }
+        {"text": `Olá ${name}!`}
       ]
    }
-   );
+  );
+});
+
+app.get('/pedido', function(request, response){
+  const {pedido} = request.headers;
+  response.status(200).json(
+    {
+      "messages": [
+        {"text": `Ok!`},
+        {"text": `Confirma o pedido?`},
+        {"text": `${pedido}`}
+      ]
+   }
+  );
 });
