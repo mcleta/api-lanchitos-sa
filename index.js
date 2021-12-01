7d0967108e9b0b7ae6e076062f1dd845e0c17413
Murillo C Leta <murillocasemiro@gmail.com>
2021-12-01 15:58:08 -0300

    Visualizando port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json())
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server's up in door 3000`));
+app.listen(process.env.PORT || 3000, () => console.log(`Server's up in door ${process.env.PORT}`));
 
 app.get("/mensagens", function(request, response){
   response.status(200).json(
