c9999cba0a46317bb041f8e448dd30d04f7ae9a9
Murillo C Leta <murillocasemiro@gmail.com>
2021-12-01 17:12:00 -0300

    Testando parte 2

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const chatFuel = require('chatfuel-api');
 
 const app = express();
 
+
 app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server's up in door ${process.env.PORT}`));
 
-app.get("/mensagens", function(request, response){
-  const {email} = request.headers
+app.get("/cardapio", function(request, response){
+  const {name} = request.headers
   response.status(200).json(
     {
       "messages": [
-        {"text": `Seu email é: ${email}` }
+        {"text": `Olá ${name}!` }
       ]
    }
    );
