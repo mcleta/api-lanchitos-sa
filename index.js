752bfefcf1b02e62c77d060761fb600b8c8e8602
Murillo C Leta <murillocasemiro@gmail.com>
2021-12-01 14:41:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const chatFuel = require('chatfuel-api');
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(3000, () => console.log(`Server's up in door 3000`));
+
+app.get("/mensagens", function(request, response){
+  response.json(
+    {
+      "messages": [
+        {"text": "Welcome to the Chatfuel Rockets!"},
+        {"text": "What are you up to?"}
+      ]
+   }
+   );
+});
