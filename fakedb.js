81981cc7241a9a25593c2b062564c385f38302a2
Murillo C Leta <murillocasemiro@gmail.com>
2021-12-02 10:14:36 -0300

    População do fakedb

---
+++ fakedb.js
@@ -0,0 +1,15 @@
+const menu = [{
+  'lanches': {
+    'xburguer': 15.5,
+    'xburguer duplo': 20.5,
+    'xegg': 14,
+    'xsalada': 12,
+    'xtudo': 25,
+    'xcalabresa': 18,
+  },
+  'bebidas': {
+    'refri': 6.5,
+    'suco natural': 4.5,
+    'agua': 3,
+  }
+}];
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server's up in door ${process.env.PORT}`));
 
