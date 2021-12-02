03a4fe011fd9f5524b876fa9367688dd25ac2371
Murillo C Leta <murillocasemiro@gmail.com>
2021-12-02 10:47:44 -0300

    Rota test para testar resposta rápida

--- index.js
+++ index.js
@@ -43,3 +43,26 @@ app.get('/endereco', function(request, response){
    }
   );
 });
+
+app.get('/test', function(req, res){
+  res.status(200).json(
+    {
+      "messages": [
+        {
+          "text":  "Did you enjoy the last game of the CF Rockets?",
+          "quick_replies": [
+            {
+              "title":"Loved it!",
+              "block_names": ["Block 1", "Block 2"]
+            },
+            {
+              "title":"Not really...",
+              "url": "https://rockets.chatfuel.com/api/sad-match",
+              "type":"json_plugin_url"
+            }
+          ]
+        }
+      ]
+    }
+  );
+});
